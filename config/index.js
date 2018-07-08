eb11a845b614b743f3f2f71ecf7adf0bbabb54e2
stupidzgd <stupidzgd@163.com>
2018-07-08 18:54:09 +0800

    express服务器配置

--- config/index.js
+++ config/index.js
@@ -51,12 +51,13 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
+    port: 9000,
 
     /**
      * Source Maps
      */
 
-    productionSourceMap: true,
+    productionSourceMap: false,
     // https://webpack.js.org/configuration/devtool/#production
     devtool: '#source-map',
 
